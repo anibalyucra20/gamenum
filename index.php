65996e5e5b4156a99f746855b8d6aa595666ff0e
anibal yucra <105953269+anibalyucra20@users.noreply.github.com>
2026-04-18 19:38:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ try {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>MathQuiz Pro</title>
+        <title>Desafío Matemático</title>
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <style>
